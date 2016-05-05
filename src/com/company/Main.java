216924edc2e0232aa504de259f2d7fcd11d08f53
Brandt Newton <btnewton@uvm.edu>
2016-05-04 23:33:01 -0400

    added main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import com.company.element.PolygonElementCreator;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        PolygonElementCreator polygonElementCreator = new PolygonElementCreator();
+
+        final double RADIUS = 3.0;
+        final double AREA = Math.PI * Math.pow(RADIUS, 2);
+
+        int n = 6;
+        while (n < 10000) {
+            double estimatedArea = polygonElementCreator.estimateAreaOfCircle(RADIUS, n);
+            System.out.println(n + " sides: " + estimatedArea + "(" + (estimatedArea / AREA * 100) + "%)");
+            n *= 2;
+        }
+
+        System.out.println("Actual area = " + AREA);
     }
 }
